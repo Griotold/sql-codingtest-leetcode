2d9b428553daa5ada2c65cce843146f2ca31eacf
Griotold <gotjd9773@naver.com>
2025-02-05 10:43:00 +0900

    [basic-joins] 9. rising-temperatures.sql.sql
    1. https://leetcode.com/problems/rising-temperature/submissions/1531690095/?envType=study-plan-v2&envId=top-sql-50

---
+++ basicjoins/rising-temperatures.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+SELECT w1.id
+FROM Weather w1
+         INNER JOIN
+     (
+         SELECT temperature, DATE_ADD(recordDate, INTERVAL 1 DAY) AS recordDate2
+         FROM Weather
+     ) w2
+     ON w1.recordDate = w2.recordDate2
+WHERE w1.temperature > w2.temperature;
